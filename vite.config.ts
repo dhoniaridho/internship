9977918d6e183727d866090317704efd83f1611e
dhoniaridho <dhoniaridho@gmail.com>
2023-09-06 20:06:20 +0700

    feat(company): add ui library

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import AutoImport from 'unplugin-auto-import/vite'
 import Layouts from 'vite-plugin-vue-layouts'
 
@@ -16,24 +17,16 @@ export default defineConfig({
     Pages({
       dirs: [
         {
-          filePatern: '**/page.*',
+          filePatern: '**/*',
           dir: 'src/app',
-          baseRoute: '',
+          baseRoute: ''
         }
       ],
       exclude: ['**/components/**'],
       importMode: 'async',
-      extensions: ['vue', 'tsx'],
-      onRoutesGenerated(routes) {
-        return routes.map((item) => {
-          return {
-            ...item,
-            path: item.path.replace(/\/page$/, '')
-          }
-        })
-      }
+      extensions: ['vue', 'tsx']
     }),
-    Components({}),
+    Components({ resolvers: [NaiveUiResolver()] }),
     AutoImport({
       include: [
         /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
